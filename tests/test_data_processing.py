1223ca819de474d3c46271b727e6f9938ec2fcc7
Estifanos Aschalew <estifaschalew@gmail.com>
2024-08-26 20:38:10 -0700

    file_path issues updated

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -5,20 +5,26 @@ from scripts.data_processing import load_data, clean_data, calculate_correlation
 class TestDataprocessing(unittest.TestCase):
     #Test if data loading works correctly for all datasets
     def test_load_data(self):
-        for file_path in['data/benin-malanville.csv','data/sierraleone-bumbuna.csv','data/togo-dapaong_qc.csv']:
+        for file_path in['C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/benin-malanville.csv',
+                         'C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/sierraleone-bumbuna.csv',
+                         'C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/togo-dapaong_qc.csv']:
             df=load_data(file_path)
             self.assertFalse(df.empty,f"{file_path} should not be empty")
     
     #Test if data cleaning works correctly for all datasets
     def test_clean_data(self):
-        for file_path in['data/benin-malanville.csv','data/sierraleone-bumbuna.csv','data/togo-dapaong_qc.csv']:
+        for file_path in['C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/benin-malanville.csv',
+                         'C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/sierraleone-bumbuna.csv',
+                         'C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/togo-dapaong_qc.csv']:
             df=load_data(file_path)
             cleaned_df=clean_data(df)
             self.assertFalse(cleaned_df.isnull().values.any(),f"{file_path} should not have any null values after cleaning")
 
     #Test if correlation calculation works correctly
     def test_calculate_correlation(self):
-        for file_path in['data/benin-malanville.csv','data/sierraleone-bumbuna.csv','data/togo-dapaong_qc.csv']:
+        for file_path in['C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/benin-malanville.csv',
+                         'C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/sierraleone-bumbuna.csv',
+                         'C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/togo-dapaong_qc.csv']:
             df=load_data(file_path)
             cleaned_df=clean_data(df)
             corr_matrix=calculate_correlation(cleaned_df)
@@ -26,7 +32,9 @@ class TestDataprocessing(unittest.TestCase):
 
     #Test if time series preparation works correctly
     def test_prepare_time_series(self):
-        for file_path in['data/benin-malanville.csv','data/sierraleone-bumbuna.csv','data/togo-dapaong_qc.csv']:
+        for file_path in['C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/benin-malanville.csv',
+                         'C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/sierraleone-bumbuna.csv',
+                         'C:/Users/Go a Head Bro/Documents/10Academy-AIM-Journey/solar-panel-strategy-analysis/data/togo-dapaong_qc.csv']:
             df=load_data(file_path)
             prepared_df=prepare_time_series(df)
             self.assertTrue(pd.api.types.is_datetime64_any_dtype(prepared_df.index), f"{file_path} should have datetime index after preparation")
